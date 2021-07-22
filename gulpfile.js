03a411e2614c874721e730d7afda677d056450fd
Sara Evans <sevans91@my.bcit.ca>
2021-07-22 13:27:04 -0700

    trying to get rid of error

--- gulpfile.js
+++ gulpfile.js
@@ -11,25 +11,25 @@
 // src() instead of gulp.src()
 const { src, dest, watch, series, parallel } = require('gulp');
 // Import Gulp plugins and npm packages that we need for this project
-const sourcemaps   = require('gulp-sourcemaps');
-const sass         = require('gulp-sass');
-const concat       = require('gulp-concat');
-const uglify       = require('gulp-uglify-es').default;
-const postcss      = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
-const cssnano      = require('cssnano');
-const replace      = require('gulp-replace');
-const browserSync  = require('browser-sync').create();
-const del          = require('del');
-const imageMin     = require('gulp-imagemin');
-const cache        = require('gulp-cache');
-const htmlMin      = require('gulp-htmlmin');
-const gulpif       = require('gulp-if');
+const cssnano = require('cssnano');
+const replace = require('gulp-replace');
+const browserSync = require('browser-sync').create();
+const del = require('del');
+const imageMin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+const htmlMin = require('gulp-htmlmin');
+const gulpif = require('gulp-if');
 
 // Dev / Build State
 // These variables are used to set the 
 // dev or build state of the Gulpfile 
-let devMode   = true;
+let devMode = true;
 let buildMode = false;
 
 // Use jQuery
@@ -39,56 +39,56 @@ let buildMode = false;
 const useJQuery = true;
 
 // File Names
-const filenames         = {};
-filenames.jQuery        = 'jquery-3.4.1.min.js';
+const filenames = {};
+filenames.jQuery = 'jquery-3.4.1.min.js';
 filenames.jsNoExtension = 'script.min';
-filenames.js            = `${filenames.jsNoExtension}.js`;
+filenames.js = `${filenames.jsNoExtension}.js`;
 
 // Folder Paths
-const folders      = {};
+const folders = {};
 // Main Folders
-folders.dev        = 'dev';
-folders.dist       = 'dist';
-folders.styles     = 'styles';
-folders.sass       = 'scss';
-folders.images     = 'images';
-folders.fonts      = 'fonts'
-folders.scripts    = 'scripts';
-folders.libs       = 'libs';
-folders.jQuery     = 'jquery';
+folders.dev = 'dev';
+folders.dist = 'dist';
+folders.styles = 'styles';
+folders.sass = 'scss';
+folders.images = 'images';
+folders.fonts = 'fonts'
+folders.scripts = 'scripts';
+folders.libs = 'libs';
+folders.jQuery = 'jquery';
 // JavaScript Folders
-folders.jsDev      = `${folders.dev}/${folders.scripts}`;
-folders.jsDist     = `${folders.dist}/${folders.scripts}`;
-folders.libsDev    = `${folders.jsDev}/${folders.libs}`;
-folders.libsDist   = `${folders.jsDistFolder}/${folders.libs}`; 
+folders.jsDev = `${folders.dev}/${folders.scripts}`;
+folders.jsDistFolder = `${folders.dist}/${folders.scripts}`;
+folders.libsDev = `${folders.jsDev}/${folders.libs}`;
+folders.libsDist = `${folders.jsDistFolder}/${folders.libs}`;
 // jQuery Folders
-folders.jQueryDev  = `${folders.libsDev}/${folders.jQuery}`;
+folders.jQueryDev = `${folders.libsDev}/${folders.jQuery}`;
 folders.jQueryDist = `${folders.libsDist}/${folders.jQuery}`;
 // Styles Folders
-folders.stylesDev  = `${folders.dev}/${folders.styles}`;
+folders.stylesDev = `${folders.dev}/${folders.styles}`;
 folders.stylesDist = `${folders.dist}/${folders.styles}`;
 // Images Folders
-folders.imagesDev  = `${folders.dev}/${folders.images}`;
+folders.imagesDev = `${folders.dev}/${folders.images}`;
 folders.imagesDist = `${folders.dist}/${folders.images}`;
 // Fonts Folders
-folders.fontsDev   = `${folders.dev}/${folders.fonts}`;
-folders.fontsDist  = `${folders.dist}/${folders.fonts}`;
+folders.fontsDev = `${folders.dev}/${folders.fonts}`;
+folders.fontsDist = `${folders.dist}/${folders.fonts}`;
 
 // File Paths
-const files  = {};
+const files = {};
 // HTML Files 
-files.html   = `${folders.dev}/**/*.html`;
+files.html = `${folders.dev}/**/*.html`;
 // Sass Files 
-files.sass   = `${folders.dev}/${folders.sass}/**/*.scss`;
+files.sass = `${folders.dev}/${folders.sass}/**/*.scss`;
 // JavaScript Files
-files.js     = `${folders.jsDev}/**/!(${filenames.jsNoExtension})*.js`;
-files.libs   = `${folders.libsDev}/**/*.js`;
+files.js = `${folders.jsDev}/**/!(${filenames.jsNoExtension})*.js`;
+files.libs = `${folders.libsDev}/**/*.js`;
 // jQuery Files
 files.jQuery = `${folders.jQueryDev}/${filenames.jQuery}`;
 // Images Files
 files.images = `${folders.imagesDev}/**/*.+(png|jpg|gif|svg)`;
 // Fonts Files
-files.fonts  = `${folders.fontsDev}/**/*`;
+files.fonts = `${folders.fontsDev}/**/*`;
 
 // Development Tasks
 
@@ -98,7 +98,7 @@ files.fonts  = `${folders.fontsDev}/**/*`;
 // 3. Injects CSS into the browser (dev mode)
 // 4. Compresses the CSS files (build mode)
 // 5. Copies CSS files to the dist styles folder
-function sassTask(){    
+function sassTask() {
   return src(files.sass)
     .pipe(gulpif(devMode, sourcemaps.init())) // Initializes sourcemaps (dev mode) 
     .pipe(sass().on('error', sass.logError)) // compiles SCSS to CSS
@@ -114,7 +114,7 @@ function sassTask(){
 // 1. Concatenates JS files
 // 2. If in build mode, compresses concatenated JS file
 // 3. If in build mode, copies concatenated and minified JS file to the dist folder 
-function jsTask(){
+function jsTask() {
   return src([files.libs, files.js, `!${files.jQuery}`])
     .pipe(gulpif(devMode, sourcemaps.init())) // Initializes sourcemaps (dev mode)
     .pipe(concat(filenames.js)) // Concatenates JS files
@@ -126,35 +126,35 @@ function jsTask(){
 
 // jQuery Task:
 // 1. If useJQuery is set to true, copies jQuery to the dist scripts folder
-function jQueryTask(done){
-  if(useJQuery === true){
+function jQueryTask(done) {
+  if (useJQuery === true) {
     src(files.jQuery)
       .pipe(dest(folders.jQueryDist)); // Copies jQuery to the dist scripts folder
   }
-  done(); 
+  done();
 }
 
 // HTML Task:
 // 1. Compresses the HTML files
 // 2. Copies the compressed HTML files to the dist folder 
-function htmlTask(){
+function htmlTask() {
   return src(files.html)
-    .pipe(htmlMin({collapseWhitespace: true})) // Compresses the HTML files
+    .pipe(htmlMin({ collapseWhitespace: true })) // Compresses the HTML files
     .pipe(dest(folders.dist)); // Copies compressed HTML files to the dist folder
 }
 
 // Images Task:
 // 1. Compresses the image files
 // 2. Copies the compressed image files to the dist images folder
-function imagesTask(){
+function imagesTask() {
   return src(files.images)
-		.pipe(cache(imageMin({interlaced: true}))) // Compresses images
-		.pipe(dest(folders.imagesDistFolder)); // Copies compressed images to the dist images folder
+    .pipe(cache(imageMin({ interlaced: true }))) // Compresses images
+    .pipe(dest(folders.imagesDistFolder)); // Copies compressed images to the dist images folder
 }
 
 // Fonts Task:
 // 1. Copies the font files to the dist fonts folder
-function fontsTask(){
+function fontsTask() {
   return src(files.fonts)
     .pipe(dest(folders.fontsDist)); // Copies the font files to the dist fonts folder
 }
@@ -162,7 +162,7 @@ function fontsTask(){
 // Cachebust Task:
 // 1. Create a time stamp based on the current time
 // 2. Replaces any query strings in the HTML files that uses the time stamp with the new timp stamp
-function cacheBustTask(){
+function cacheBustTask() {
   const cbString = new Date().getTime(); // Create time stamp based on current time
   return src(files.html)
     .pipe(replace(/cb=\d+/g, 'cb=' + cbString)) // Replace current time stamp query string in the HTML files with the new time stamp
@@ -173,7 +173,7 @@ function cacheBustTask(){
 // 1. Watch for changes in the dev Sass directory, if a change is detected then it runs the Sass task
 // 2. Watch for changes in the dev JS directory, if a change is detected then it runs the JS task
 // 3. Watch for changes in the HTML files in the dev directory, if a change is deteched then it re-loads the browser 
-function watchTask(){
+function watchTask() {
   // Setup Browsersync for automatic reloading and re-freshing of CSS, Javascript and HTML
   browserSync.init({
     server: {
@@ -190,7 +190,7 @@ function watchTask(){
 // Set Build State Task:
 // 1. Sets devMode to false
 // 2. Sets buildMode to true
-function setBuildState(done){
+function setBuildState(done) {
   devMode = false;
   buildMode = true;
   done();
@@ -198,7 +198,7 @@ function setBuildState(done){
 
 // Clean Task: 
 // 1. Deletes all folders and files in the dist folder
-function cleanTask(done){
+function cleanTask(done) {
   del.sync(folders.dist);
   done();
 }
