46504e562a5cfbb82fc7d207c1e58f58a272e096
Chris Coulson <chrisjamescoulson@gmail.com>
2021-07-25 17:22:48 -0700

    finished styling header nav, dropdown and animation working

---
+++ .history/dev/scripts/script_20210725145315.js
@@ -0,0 +1,8 @@
+// Write your JavaScript here...
+'use strict';
+
+$(".menu-toggle").click(function () {
+    var navigation = document.getElementById('head-nav-list');
+    navigation.classList.toggle('openMenu');
+
+});
